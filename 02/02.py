e7400c5c03b59213ade2b946ab83d3365da6a5f6
Wilmer Ansgariusson <wansgariusson@gmail.com>
2020-12-02 06:20:19 +0100

    Day 2

---
+++ 02/02.py
@@ -0,0 +1,19 @@
+import sys
+counta = 0
+countb = 0
+for line in sys.stdin.readlines():
+    sp = line.split(" ")
+    letter = sp[1][0]
+    passw = sp[-1]
+    bound = sp[0].split("-")
+    if passw.count(letter) >= int(bound[0]) and passw.count(letter) <= int(bound[1]):
+        counta += 1
+    occ = 0
+    if passw[int(bound[0])-1] == letter:
+        occ += 1
+    if passw[int(bound[1])-1] == letter:
+        occ += 1
+    if occ == 1:
+        countb += 1
+print(counta)
+print(countb)
