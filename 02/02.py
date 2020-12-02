faf6f769a290d444b7ef81214980ec098ad4e2e6
Wilmer Ansgariusson <wansgariusson@gmail.com>
2020-12-02 06:22:39 +0100

    Minor refactor

--- 02/02.py
+++ 02/02.py
@@ -5,13 +5,13 @@ for line in sys.stdin.readlines():
     sp = line.split(" ")
     letter = sp[1][0]
     passw = sp[-1]
-    bound = sp[0].split("-")
-    if passw.count(letter) >= int(bound[0]) and passw.count(letter) <= int(bound[1]):
+    bound = [int(x) for x in sp[0].split("-")]
+    if passw.count(letter) >= bound[0] and passw.count(letter) <= bound[1]:
         counta += 1
     occ = 0
-    if passw[int(bound[0])-1] == letter:
+    if passw[bound[0]-1] == letter:
         occ += 1
-    if passw[int(bound[1])-1] == letter:
+    if passw[bound[1]-1] == letter:
         occ += 1
     if occ == 1:
         countb += 1
