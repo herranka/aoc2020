2dc1c497f67f1cc7404648ad79d8e4443987ee28
Wilmer Ansgariusson <wansgariusson@gmail.com>
2020-12-21 16:40:45 +0100

    Refactor day 14 part 1

--- 14/14.py
+++ 14/14.py
@@ -1,35 +1,6 @@
 import sys
 lines = [line.strip() for line in sys.stdin.readlines()]
 
-def apply_mask(val, mask):
-    binary = format(val, '036b')
-    out = []
-    for i in range(len(mask)):
-        if mask[i] == "1":
-            out.append("1")
-        elif mask[i] == "0":
-            out.append("0")
-        else:
-            out.append(binary[i])
-    return int(''.join(out), 2)
-
-def store(val, addr, mask, mem):
-    mem[addr] = apply_mask(val,mask)
-
-mask = "XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX"
-mem = [0 for i in range(100000)]
-for line in lines:
-    var, val = line.split(" = ")
-    if var == "mask":
-        mask = val
-    else:
-        address = int(var[4:-1])
-        val = int(val)
-        store(val, address, mask, mem)
-print(sum(mem))
-
-# part 2
-
 def masks(st):
     fl = 0
     fi = 0
@@ -39,6 +10,21 @@ def masks(st):
         elif ch == "X":
             fl += 1<<(len(st)-i-1)
     return fl, fi
+
+floating = 0
+fixed = 0
+mem = [0 for i in range(100000)]
+for line in lines:
+    var, val = line.split(" = ")
+    if var == "mask":
+        floating, fixed = masks(val)
+    else:
+        address = int(var[4:-1])
+        val = int(val)
+        mem[address] = (fixed & ~floating) | (floating & val)
+print(sum(mem))
+
+# part 2
 
 def subsets(mask):
     subset = mask
