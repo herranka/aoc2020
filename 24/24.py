11bcbc96aa32467d9bb4bf5279333d84fe2b02e3
Wilmer Ansgariusson <wansgariusson@gmail.com>
2020-12-24 23:14:09 +0100

    Day 24 part 2

--- 24/24.py
+++ 24/24.py
@@ -1,4 +1,5 @@
 import sys
+from copy import copy
 
 instructions = [x.strip() for x in sys.stdin.readlines()]
 
@@ -6,25 +7,25 @@ directions = {"ne", "nw", "se", "sw", "w", "e"}
 
 def move(x,y,st):
     if st == "w":
-        return  [x-1, y]
+        return  (x-1, y)
     elif st == "e":
-        return [x+1, y]
+        return (x+1, y)
     elif st == "nw":
-        return [x, y-1]
+        return (x, y-1)
     elif st == "ne":
-        return [x+1,y-1]
+        return (x+1,y-1)
     elif st == "sw":
-        return [x-1, y+1]
+        return (x-1, y+1)
     elif st == "se":
-        return [x, y+1]
+        return (x, y+1)
 
-def flip(black, x, y):
-    if (x,y) in black:
-        black.remove((x,y))
+def flip(bl, x, y):
+    if (x,y) in bl:
+        bl.remove((x,y))
     else:
-        black.add((x,y))
+        bl.add((x,y))
 
-origin = [50,50]
+origin = (50,50)
 black = set()
 
 for line in instructions:
@@ -45,3 +46,20 @@ print(len(black))
 
 def neighbours(x, y):
     return (move(x,y,m) for m in directions)
+
+def black_neigh(bl, x,y):
+    return sum(1 for pos in neighbours(x,y) if pos in bl)
+
+day = 1
+while day <= 100:
+    blcopy = copy(black)
+    for x,y in blcopy:
+        count = black_neigh(blcopy, x, y)
+        if count == 0 or count > 2:
+            black.remove((x,y))
+        for nx,ny in neighbours(x,y):
+            ncount = black_neigh(blcopy, nx, ny)
+            if ncount == 2:
+                black.add((nx,ny))
+    day += 1
+print(len(black))
